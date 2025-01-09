9d0a9934a865e05f94df3b82405fd522f541b034
admin <admin@A006-00423.local>
2025-01-10 00:02:16 +0500

    update

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dbConnection from "./config/dbConnection.js";
+import cors from "cors";
+// import errorHandler from "./middleware/errosHandler";
+import env from "dotenv";
+import Router from "./routes/contactsRoutes.js";
+import userRoute from "./routes/userRoutes.js";
+dbConnection();
+const app = express();
+const envirement = env.config();
+app.use(cors());
+const port = process.env.PORT || 5001;
+app.use(express.static("dist"));
+app.use(express.json());
+app.use("/api/contacts", Router);
+app.use("/", (req, res) => {
+  res.send("fullstack web app");
+});
+// app.use("/api/users", userRoute);
+// app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log("server is runing on", port);
+});
