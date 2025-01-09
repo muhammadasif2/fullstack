159f6fbd52236d5d963a59a678f953b6c4d17abb
admin <admin@A006-00423.local>
2025-01-10 00:52:51 +0500

    added gitignore

--- server/index.js
+++ server/index.js
@@ -1,24 +1,50 @@
+// import express from "express";
+// import dbConnection from "./config/dbConnection.js";
+// import cors from "cors";
+// // import errorHandler from "./middleware/errosHandler";
+// import env from "dotenv";
+// import Router from "./routes/contactsRoutes.js";
+// import userRoute from "./routes/userRoutes.js";
+// dbConnection();
+// const app = express();
+// const envirement = env.config();
+// app.use(cors());
+// const port = process.env.PORT || 5001;
+// app.use(express.json());
+// app.use("/api/contacts", Router);
+// app.use("/", (req, res) => {
+//   res.send("fullstack web app");
+// });
+// // app.use("/api/users", userRoute);
+// // app.use(errorHandler);
+
+// app.listen(port, () => {
+//   console.log("server is runing on", port);
+// });
+
 import express from "express";
 import dbConnection from "./config/dbConnection.js";
 import cors from "cors";
-// import errorHandler from "./middleware/errosHandler";
 import env from "dotenv";
 import Router from "./routes/contactsRoutes.js";
 import userRoute from "./routes/userRoutes.js";
+
+// Set up environment and DB connection
 dbConnection();
 const app = express();
-const envirement = env.config();
+const environment = env.config();
+
+// Middleware
 app.use(cors());
-const port = process.env.PORT || 5001;
 app.use(express.static("dist"));
 app.use(express.json());
+
+// Routes
 app.use("/api/contacts", Router);
 app.use("/", (req, res) => {
-  res.send("fullstack web app");
+  res.send("Fullstack Web App");
 });
 // app.use("/api/users", userRoute);
-// app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log("server is runing on", port);
-});
+// Export app for Vercel
+export default app;
